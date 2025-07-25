3cb1ebba79d19c5e32a0f058e2e9dcb6b78b7d3e
crmclicon <beniteznegocios@gmail.com>
2025-07-25 16:31:11 -0300

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,20 +5,26 @@ import { getStorage } from "firebase-admin/storage";
 initializeApp();
 
 export const crearCarpetasEmpresa = onDocumentCreated("empresas/{empresaId}", async (event) => {
-  // const snap = event.data as QueryDocumentSnapshot;
-  const empresaId = event.params.empresaId;
+  const snap = event.data;
+  const empresaData = snap?.data();
+  const nombreEmpresa = empresaData?.nombre;
+
+  if (!nombreEmpresa || typeof nombreEmpresa !== "string") {
+    console.error("No se encontró un 'nombre' válido en el documento");
+    return;
+  }
 
   const storage = getStorage().bucket();
 
   const carpetas = [
-    `empresas/${empresaId}/historial_carteras/.init`,
-    `empresas/${empresaId}/historial_ventas/.init`,
-    `empresas/${empresaId}/documentacion_clientes/.init`,
+    `empresas/${nombreEmpresa}/historial_carteras/.init`,
+    `empresas/${nombreEmpresa}/historial_ventas/.init`,
+    `empresas/${nombreEmpresa}/documentacion_clientes/.init`,
   ];
 
   await Promise.all(
     carpetas.map(path => storage.file(path).save("inicial"))
   );
 
-  console.log(`Carpetas creadas para empresa ${empresaId}`);
+  console.log(`Carpetas creadas para empresa ${nombreEmpresa}`);
 });
